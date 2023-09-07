155446c3f8e9f74efb1d267778d2205621208d66
Hindrik Sjieuwe de Hoop <114724216+HSdeH@users.noreply.github.com>
2023-09-07 16:07:44 +0200

    Vec half ingevuld

--- project_eend/src/main/java/classes/Vec.java
+++ project_eend/src/main/java/classes/Vec.java
@@ -1,4 +1,22 @@
 package classes;
 
 public class Vec {
+    // vectoren en (?)kleur
+    private double[] d = new double[3]; //4e dimensie (alfa voor kleur)
+    public Vec(){
+        for (int i = 0; i <3; i++){
+            d[i]=0;
+        }
+    }// misschien beter om meteen by de initialisatie al {0,0,0} te maken
+    public Vec(double x, double y, double z){
+        d[0]=x;
+        d[1]=y;
+        d[2]=z;
+    }
+    public double x() {return d[0];}
+    public double y() {return d[1];}
+    public double z() {return d[2];}
+    public Vec inverse(){
+        return new Vec(-d[0],-d[1],-d[2]);
+    }
 }
