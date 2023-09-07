38b63664d02876f2137ea23d33ae0a67496eea29
Hindrik Sjieuwe de Hoop <114724216+HSdeH@users.noreply.github.com>
2023-09-07 13:37:01 +0200

    placeholders

--- project_eend/src/main/java/com/example/project_eend/HelloApplication.java
+++ project_eend/src/main/java/com/example/project_eend/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.project_eend;
 
+import classes.Vec;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -19,5 +21,6 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+        Vec Vector = new Vec();
     }
 }
