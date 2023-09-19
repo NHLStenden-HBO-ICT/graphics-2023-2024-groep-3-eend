9be1532ff17e8816e5c627a75bb92f8be71760a3
joelsefanja <joelsefanja@gmail.com>
2023-09-19 17:11:31 +0200

    Renamed vector to vector old. Because vec will become vector.

--- project_eend/src/test/java/classes/VectorTest.java
+++ project_eend/src/test/java/classes/VectorTest.java
@@ -9,7 +9,7 @@ class VectorTest {
     @Test
     void getX_expectX() {
         double expected = 1.4;
-        Vector vector = new Vector(1.4,2.6,-3.5);
+        VectorOld vector = new VectorOld(1.4,2.6,-3.5);
         double actual = vector.getX();
         assertEquals(expected, actual);
 
@@ -18,7 +18,7 @@ class VectorTest {
     @Test
     void getY_expectY() {
         double expected = 3.5;
-        Vector vector = new Vector(2.2,3.5,6.3);
+        VectorOld vector = new VectorOld(2.2,3.5,6.3);
         double actual = vector.getY();
         assertEquals(expected, actual);
 
@@ -27,7 +27,7 @@ class VectorTest {
     @Test
     void getZ_expectZ() {
         double expected = 3.5;
-        Vector vector = new Vector(2.2,3.5,6.3);
+        VectorOld vector = new VectorOld(2.2,3.5,6.3);
         double actual = vector.getY();
         assertEquals(expected, actual);
     }
@@ -35,7 +35,7 @@ class VectorTest {
     @Test
     void getCoordinates_expectAllCoordinates() {
         double[] expected = {0.1, 2.4, -1.5};
-        Vector vector = new Vector(0.1, 2.4, -1.5);
+        VectorOld vector = new VectorOld(0.1, 2.4, -1.5);
         double[] actual = vector.getCoordinates();
         assertArrayEquals(expected, actual);
 
@@ -43,56 +43,56 @@ class VectorTest {
 
     @Test
     void inverse_expectInverse() {
-       Vector vector = new Vector(3,4,5);
-       Vector expected = new Vector(-3,-4,-5);
-       Vector actual = vector.inverse();
+       VectorOld vector = new VectorOld(3,4,5);
+       VectorOld expected = new VectorOld(-3,-4,-5);
+       VectorOld actual = vector.inverse();
        assertArrayEquals(expected.getCoordinates(), actual.getCoordinates());
     }
 
     @Test
     void add_expectSum() {
-        Vector vectorA = new Vector(1,2,3);
-        Vector vectorB = new Vector(-3.5,-4.4,-3.5);
-        Vector expected = new Vector(-2.5, -2.4, -0.5);
-        Vector actual = Vector.add(vectorA, vectorB);
+        VectorOld vectorA = new VectorOld(1,2,3);
+        VectorOld vectorB = new VectorOld(-3.5,-4.4,-3.5);
+        VectorOld expected = new VectorOld(-2.5, -2.4, -0.5);
+        VectorOld actual = VectorOld.add(vectorA, vectorB);
         assertArrayEquals(expected.getCoordinates(), actual.getCoordinates(), 0.001);
 
     }
 
     @Test
     void subtract_expectDifference() {
-        Vector vectorA = new Vector(5.4,2.3,-3.4);
-        Vector vectorB = new Vector(-54.3,-10.3,4.5);
-        Vector expected = new Vector(59.7, 12.6, -7.9);
-        Vector actual = Vector.subtract(vectorA, vectorB);
+        VectorOld vectorA = new VectorOld(5.4,2.3,-3.4);
+        VectorOld vectorB = new VectorOld(-54.3,-10.3,4.5);
+        VectorOld expected = new VectorOld(59.7, 12.6, -7.9);
+        VectorOld actual = VectorOld.subtract(vectorA, vectorB);
         assertArrayEquals(expected.getCoordinates(), actual.getCoordinates(), 0.001);
     }
 
     @Test
     void scale_expectScaled() {
-        Vector vector = new Vector(5.4,2.3,-3.4);
-        Vector expected = new Vector(8.1, 3.45, -4.6);
-        Vector actual = vector.scale(1.5);
+        VectorOld vector = new VectorOld(5.4,2.3,-3.4);
+        VectorOld expected = new VectorOld(8.1, 3.45, -4.6);
+        VectorOld actual = vector.scale(1.5);
         assertArrayEquals(expected.getCoordinates(), actual.getCoordinates(), 0.001);
     }
 
     @Test
     void getDotProduct_expectDotProduct() {
-        Vector vectorA = new Vector(5,4,13);
-        Vector vectorB = new Vector(2,5,10);
+        VectorOld vectorA = new VectorOld(5,4,13);
+        VectorOld vectorB = new VectorOld(2,5,10);
         double[] coordsA = vectorA.getCoordinates();
         double[] coordsB = vectorB.getCoordinates();
         double expected = 160;
-        double actual = Vector.GetDotProduct(vectorA, vectorB);
+        double actual = VectorOld.GetDotProduct(vectorA, vectorB);
         assertEquals(expected, actual);
     }
 
     @Test
     void getCrossProduct_expectCrossProduct() {
-        Vector vectorA = new Vector(3, -1, 2);
-        Vector vectorB = new Vector(0, 2, -3);
-        Vector expected = new Vector(-1, 9, 6);
-        Vector actual = Vector.GetCrossProduct(vectorA, vectorB);
+        VectorOld vectorA = new VectorOld(3, -1, 2);
+        VectorOld vectorB = new VectorOld(0, 2, -3);
+        VectorOld expected = new VectorOld(-1, 9, 6);
+        VectorOld actual = VectorOld.GetCrossProduct(vectorA, vectorB);
         assertArrayEquals(expected.getCoordinates(), actual.getCoordinates());
 
     }
@@ -100,8 +100,8 @@ class VectorTest {
     @Test
     void getLength_expectLength() {
         double expected = 7.43;
-        Vector vector = new Vector(3, 4, -5.5);
-        double actual = vector.GetLenght();
+        VectorOld vector = new VectorOld(3, 4, -5.5);
+        double actual = vector.GetLength();
         assertEquals(expected, actual, 0.01);
     }
 }
