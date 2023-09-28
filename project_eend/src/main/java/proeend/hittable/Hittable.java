6796e014aef6841ca9e2a7874fe1537848f4b03e
joelsefanja <joelsefanja@gmail.com>
2023-09-28 15:55:48 +0200

    Docs in Hitrecord en Hittable

--- project_eend/src/main/java/proeend/hittable/Hittable.java
+++ project_eend/src/main/java/proeend/hittable/Hittable.java
@@ -5,21 +5,52 @@ import proeend.misc.HitRecord;
 import proeend.math.Interval;
 import proeend.math.Ray;
 
+/**
+ * Abstracte klasse die dient als basis voor hittable objecten in een 3D-scène.
+ * Hittable objecten kunnen worden geraakt door een lichtstraal (Ray).
+ */
 public abstract class Hittable {
+
+    /**
+     * Bepaalt of een lichtstraal (Ray) het hittable object raakt en berekent de raakpunten.
+     *
+     * @param ray   De lichtstraal die wordt getest op botsing.
+     * @param rayT  Een interval dat de geldige tijdstippen van de lichtstraal vertegenwoordigt.
+     * @param rec   Een HitRecord-object om informatie over de botsing op te slaan.
+     * @return      True als de lichtstraal het object raakt, anders false.
+     */
     public abstract boolean hit(Ray ray, Interval rayT, HitRecord rec);
 
-    private Material material;
+    private Material material; // Het materiaal van het hittable object.
 
+    /**
+     * Constructor voor een hittable object zonder een gespecificeerd materiaal.
+     */
     public Hittable(){}
 
+    /**
+     * Constructor voor een hittable object met een gespecificeerd materiaal.
+     *
+     * @param material  Het materiaal van het hittable object.
+     */
     public Hittable(Material material){
         this.material = material;
     }
 
+    /**
+     * Haal het materiaal op dat aan dit hittable object is gekoppeld.
+     *
+     * @return  Het materiaal van het hittable object.
+     */
     public Material getMaterial() {
         return material;
     }
 
+    /**
+     * Stel het materiaal in dat aan dit hittable object is gekoppeld.
+     *
+     * @param material  Het nieuwe materiaal voor het hittable object.
+     */
     public void setMaterial(Material material) {
         this.material = material;
     }
--- project_eend/src/main/java/proeend/misc/HitRecord.java
+++ project_eend/src/main/java/proeend/misc/HitRecord.java
@@ -4,20 +4,47 @@ import proeend.material.Material;
 import proeend.math.Ray;
 import proeend.math.Vector;
 
+/**
+ * Een klasse die informatie opslaat over een botsing (hit) tussen een lichtstraal en een object in een 3D-scène.
+ */
 public class HitRecord {
+    /** Het raakpunt (punt van impact) van de lichtstraal met het object. */
     public Vector p;
+
+    /** De normaalvector op het raakpunt van het oppervlak van het object. */
     public Vector normal;
+
+    /** De parameter 't' die de afstand vertegenwoordigt van het beginpunt van de lichtstraal tot het raakpunt. */
     public double t;
+
+    /** Geeft aan of het frontale oppervlak van het object wordt geraakt. */
     public boolean frontFace;
-    //voor driehoeken en textures
-    public double u,v,w = 0.0;
+
+    /**
+     * De textuurcoördinaten 'u' en 'v' op het raakpunt van het oppervlak van het object.
+     * Deze waarden worden vaak gebruikt bij texture mapping.
+     */
+    public double u, v;
+
+    /** Het materiaal van het object waarop is gebotst. */
     public Material material;
-    //outwardNormal moet eenheidsvector zijn
+
+    /**
+     * Bepaalt de normaalvector op basis van de richting van de lichtstraal en de uitgaande normaalvector.
+     *
+     * @param ray           De lichtstraal die is gebruikt voor de botsing.
+     * @param outwardNormal De uitgaande normaalvector op het raakpunt van het oppervlak.
+     */
     public void setFaceNormal(Ray ray, Vector outwardNormal) {
         frontFace = Vector.dot(ray.direction, outwardNormal) < 0;
-        normal = frontFace ? outwardNormal: Vector.inverse(outwardNormal);
+        normal = frontFace ? outwardNormal : Vector.inverse(outwardNormal);
     }
 
+    /**
+     * Kopieert gegevens van een ander HitRecord-object naar dit object.
+     *
+     * @param rec Het HitRecord-object waarnaar de gegevens worden gekopieerd.
+     */
     public void copy(HitRecord rec) {
         this.p = rec.p;
         this.normal = rec.normal;
