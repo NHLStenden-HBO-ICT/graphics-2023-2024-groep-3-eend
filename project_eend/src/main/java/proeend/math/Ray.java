beb02c30bf24e0d73fac0666b73ce5a324cfd93f
joelsefanja <joelsefanja@gmail.com>
2023-09-29 11:47:37 +0200

    java doc toegevoegd aan Ray

--- project_eend/src/main/java/proeend/math/Ray.java
+++ project_eend/src/main/java/proeend/math/Ray.java
@@ -1,18 +1,51 @@
+/**
+ * De Ray-klasse vertegenwoordigt een straal in de ruimte, gedefinieerd door een oorsprong (origin) en een richting (direction).
+ */
 package proeend.math;
 
 public class Ray {
-
+    /** De oorsprong van de straal. */
     public Vector origin;
+
+    /** De richting van de straal. */
     public Vector direction;
+
+    /**
+     * Constructor voor het maken van een straal met opgegeven oorsprong en richting.
+     *
+     * @param origin    De oorsprong van de straal.
+     * @param direction De richting van de straal.
+     */
     public Ray(Vector origin, Vector direction) {
         this.origin = origin;
         this.direction = direction;
     }
-    public Vector origin() {return origin;}
-    public Vector direction() {return direction;}
-    //geeft de vector terug die de ray bij die t bereikt
+
+    /**
+     * Geeft de oorsprong van de straal terug.
+     *
+     * @return De oorsprong van de straal.
+     */
+    public Vector origin() {
+        return origin;
+    }
+
+    /**
+     * Geeft de richting van de straal terug.
+     *
+     * @return De richting van de straal.
+     */
+    public Vector direction() {
+        return direction;
+    }
+
+    /**
+     * Geeft de vector terug die de straal bereikt op een bepaalde waarde van t.
+     *
+     * @param t De waarde van t waarmee de positie langs de straal wordt berekend.
+     * @return De vector die de straal bereikt op de opgegeven waarde van t.
+     */
     public Vector at(double t) {
         return Vector.add(origin, Vector.scale(t, direction));
     }
-
 }
