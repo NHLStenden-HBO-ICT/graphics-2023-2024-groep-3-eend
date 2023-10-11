809be00c1ad3c289e35735f1ac3c68ab417fb171
joelsefanja <joelsefanja@gmail.com>
2023-10-11 23:15:58 +0200

    Leeg test project toegevoegd, debuggen gaat toch wel gemakkelijker als je weet waar de fout zit ;)

---
+++ project_eend/src/test/java/com/proeend/hittable/Quad.java
@@ -0,0 +1,12 @@
+package com.proeend.hittable;
+
+/*
+
+public class Quad extends Hittable{
+
+    @Override
+    public boolean hit(Ray ray, Interval rayT, HitRecord rec) {
+        return false;
+    }
+}
+*/
